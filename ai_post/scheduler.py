1ecc44b0edeeb424cb38d086a0687248a61750c6
baskduf <baskduf@ynu.ac.kr>
2025-05-31 21:08:17 +0900

    lock scheduler

--- ai_post/scheduler.py
+++ ai_post/scheduler.py
@@ -5,12 +5,13 @@ import requests
 from apscheduler.schedulers.background import BackgroundScheduler
 
 def call_auto_generate_post():
-    url = "http://localhost:8000/ai_post/post/"
-    try:
-        response = requests.post(url, timeout=500)
-        print(f"[스케줄러] auto_generate_post 호출 성공, 상태 코드: {response.status_code}")
-    except Exception as e:
-        print(f"[스케줄러] auto_generate_post 호출 실패: {e}")
+    # url = "http://localhost:8000/ai_post/post/"
+    # try:
+    #     response = requests.post(url, timeout=500)
+    #     print(f"[스케줄러] auto_generate_post 호출 성공, 상태 코드: {response.status_code}")
+    # except Exception as e:
+    #     print(f"[스케줄러] auto_generate_post 호출 실패: {e}")
+    print("test")
 
 def start():
     scheduler = BackgroundScheduler()
@@ -18,7 +19,7 @@ def start():
     from datetime import datetime
 
     #scheduler.add_job(call_auto_generate_post, 'interval', minutes=30, next_run_time=datetime.now())
-    scheduler.add_job(call_auto_generate_post, 'interval', minutes=30)
+    scheduler.add_job(call_auto_generate_post, 'interval', minutes=800)
     scheduler.start()
 
     jobs = scheduler.get_jobs()
